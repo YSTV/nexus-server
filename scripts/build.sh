e3530a7b9cf0db9cc86c7218598907b7910396cb
Sam Willcocks <sam@wlcx.cc>
2017-01-05 16:14:48 +0000

    Update godeps, including local version of migrate

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 go get github.com/mitchellh/gox
-gox -ldflags="-X main.VERSION=`git describe --tags`" -output "dist/nexus-server_{{.OS}}_{{.Arch}}" -osarch="linux/amd64 linux/arm darwin/amd64 freebsd/amd64 windows/amd64"
+gox -ldflags="-X main.VERSION=`git describe --tags`" -output "dist/nexus-server_{{.OS}}_{{.Arch}}" -osarch="linux/386 linux/amd64 linux/arm darwin/amd64 freebsd/amd64 windows/amd64"
 
